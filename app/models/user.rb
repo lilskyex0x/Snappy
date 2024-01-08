c01efab0bd3c7686106df844649da0a0b799ffdf
unknown <lilskyex0x.apk@gmail.com>
2024-01-08 17:52:43 +0630

    Installed rubocop and stylelint

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-    has_many :entities, foreign_key: :author_id
-    has_many :groups, through: :entities
+  has_many :entities, foreign_key: :author_id
+  has_many :groups, through: :entities
 end
