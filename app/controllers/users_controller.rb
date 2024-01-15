c4e67307db5afdbec42c8e5d429023dd15e662eb
Aung Pyae Khant <108471064+lilskyex0x@users.noreply.github.com>
2024-01-15 22:01:46 +0630

    Fix rubocop errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
-    def show
-      @user = User.find_by_id(params[:id])
-  
-      if @user.nil?
-        redirect_to home_path
-      end
-    end
+  def show
+    @user = User.find_by_id(params[:id])
+
+    return unless @user.nil?
+
+    redirect_to home_path
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
 
   has_many :authored_categories, class_name: 'Category', foreign_key: 'author_id'
   has_many :authored_transactions, class_name: 'Transaction', foreign_key: 'author_id'
-  
+
   validates :name, presence: true
 end
