e986d10c18330d301c7660e61198f05ce0243a21
Aung Pyae Khant <108471064+lilskyex0x@users.noreply.github.com>
2024-01-16 16:41:07 +0630

    Modified category model relation with transactions

--- app/models/categories_transaction.rb
+++ app/models/categories_transaction.rb
@@ -1,6 +1,6 @@
 class CategoriesTransaction < ApplicationRecord
   belongs_to :category
-  belongs_to :transaction, foreign_key: 'transaction_ref_id'
+  belongs_to :transaction_ref, class_name: 'Transaction', foreign_key: 'transaction_ref_id'
 
   validates :category_id, presence: true
   validates :transaction_ref_id, presence: true
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :categories_transactions
-  has_many :transactions, through: :categories_transactions
+  has_many :transactions, through: :categories_transactions, source: :transaction_ref
 
   validates :name, presence: true
   validates :icon, presence: true
