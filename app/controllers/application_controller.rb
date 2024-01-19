321b7a8e452388f3567839970d220308f02b1d55
Aung Pyae Khant <108471064+lilskyex0x@users.noreply.github.com>
2024-01-19 19:05:05 +0630

    Modified application controller for faster loading application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_cache_headers
 
   protected
 
@@ -10,5 +11,15 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(_resource)
     categories_path
+  end
+
+  private
+
+  def set_cache_headers
+    return unless current_user
+
+    response.headers['Cache-Control'] = 'no-cache, no-store, max-age=0, must-revalidate'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = 'Fri, 01 Jan 1990 00:00:00 GMT'
   end
 end
