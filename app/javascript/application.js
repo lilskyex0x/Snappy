a64931f698be23244a98ce6279369f5226577052
Aung Pyae Khant <108471064+lilskyex0x@users.noreply.github.com>
2024-01-19 19:06:34 +0630

    Created javascript code for next button in navbar to work for login, sign up, and etc..

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,23 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener('turbo:load', () => {
+  const nextButton = document.getElementById('next-button');
+  const form = document.querySelector('section.app-body form');
+
+  if (nextButton) {
+    nextButton.addEventListener('click', function() {
+      form.submit();
+    });
+  }
+
+  if (form) {
+    form.addEventListener('keydown', function(event) {
+      if (event.key === 'Enter') {
+        event.preventDefault();
+        form.submit();
+      }
+    });
+  }
+});
