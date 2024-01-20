e11b6f76526652a93d2ef71a5bd06604c8cddd3d
Aung Pyae Khant <108471064+lilskyex0x@users.noreply.github.com>
2024-01-20 17:59:07 +0630

    Added require validation in categories transaction model

--- app/models/categories_transaction.rb
+++ app/models/categories_transaction.rb
@@ -1,4 +1,7 @@
 class CategoriesTransaction < ApplicationRecord
   belongs_to :category
   belongs_to :transaction_record, class_name: 'Transaction', foreign_key: 'transaction_ref_id'
+
+  validates :category_id, presence: true
+  validates :transaction_ref_id, presence: true
 end
